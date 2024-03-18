b908e8e4095e88942908ffe0a8a9fea9b8331f64
Sushen Biswas <sushenbiswasaga@gmail.com>
2024-03-18 21:24:36 +0600

    Update: main.py "Script work fine. We have to find Chart Data"

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ import re
 from selenium.webdriver.common.by import By
 from driver.driver import Driver
 
-Driver().driver.get("https://www.binance.com/en/trading-bots")
+driver = Driver().driver
 
+driver.get("https://www.binance.com/en/trading-bots")
+
+driver.implicitly_wait(10)
+time.sleep(4)
+driver.find_element(By.XPATH, ("//div[contains(.,'SOLUSDT Perpetual')]/ancestor::div[contains(@class,'css-1fymml5')]")).click()
+
+input("End:")
