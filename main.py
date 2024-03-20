ad538054dd117d3206c2ae46fd3ea7c256054652
Sushen Biswas <sushenbiswasaga@gmail.com>
2024-03-20 12:16:38 +0600

    Update: main.py "We find the elements"

--- main.py
+++ main.py
@@ -9,7 +9,25 @@ driver = Driver().driver
 driver.get("https://www.binance.com/en/trading-bots")
 
 driver.implicitly_wait(10)
-time.sleep(4)
+time.sleep(1)
 driver.find_element(By.XPATH, ("//div[contains(.,'SOLUSDT Perpetual')]/ancestor::div[contains(@class,'css-1fymml5')]")).click()
 
-input("End:")
+# input("Find Create Futures Grid Window :")
+
+driver.implicitly_wait(10)
+time.sleep(1)
+header_element = driver.find_element(By.XPATH, ("//div[contains(text(),'Create Futures Grid')]"))
+header_element.click()
+print(header_element.text)
+
+# input("Find Chart Elements:")
+
+driver.implicitly_wait(10)
+time.sleep(1)
+chart_elements = driver.find_elements(By.XPATH, ("//div[@data-highcharts-chart]"))
+for element in chart_elements:
+    if len(element.text)> 1:
+        print(element.text)
+        print(element.get_attribute("outerHTML"))
+        input("Find Something:")
+
