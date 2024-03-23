65cd2d09cf225027789c2bc8d6816aaad5e15b04
forhad <forhad1822@gmail.com>
2024-03-23 12:14:23 +0600

    getting svg code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import re
+from bs4 import BeautifulSoup
 
 from selenium.webdriver.common.by import By
 from driver.driver import Driver
@@ -10,13 +11,14 @@ driver.get("https://www.binance.com/en/trading-bots")
 
 driver.implicitly_wait(10)
 time.sleep(1)
-driver.find_element(By.XPATH, ("//div[contains(.,'SOLUSDT Perpetual')]/ancestor::div[contains(@class,'css-1fymml5')]")).click()
+driver.find_element(By.XPATH,
+                    "(//div[@class='css-1fymml5'])[2]").click()
 
 # input("Find Create Futures Grid Window :")
 
 driver.implicitly_wait(10)
 time.sleep(1)
-header_element = driver.find_element(By.XPATH, ("//div[contains(text(),'Create Futures Grid')]"))
+header_element = driver.find_element(By.XPATH, "//div[contains(text(),'Create Futures Grid')]")
 header_element.click()
 print(header_element.text)
 
@@ -24,10 +26,18 @@ print(header_element.text)
 
 driver.implicitly_wait(10)
 time.sleep(1)
-chart_elements = driver.find_elements(By.XPATH, ("//div[@data-highcharts-chart]"))
+chart_elements = driver.find_elements(By.XPATH, "//div[@data-highcharts-chart]")
 for element in chart_elements:
-    if len(element.text)> 1:
-        print(element.text)
-        print(element.get_attribute("outerHTML"))
+    if len(element.text) > 1:
+        # print(element.text)
+        svg_html = element.get_attribute("outerHTML")
+        match = re.search(r'<svg[^>]*>(.*?)</svg>', svg_html, re.DOTALL)
+        svg = match.group(0)
+        print(svg)
+        # soup = BeautifulSoup(svg, 'html.parser')
+        #
+        # text_elements = soup.find_all('text')
+        # # Extract text content from <text> elements
+        # text_values = [text.get_text() for text in text_elements]
+        # print(text_values)
         input("Find Something:")
-
